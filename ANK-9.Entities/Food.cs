037f9cdfc2de78fae26e566b2bd1cb1482405ad5
lenovo <lenovo@LAPTOP-8GNSB8G4>
2022-11-29 11:59:32 +0300

    Add project files.

---
+++ ANK-9.Entities/Food.cs
@@ -0,0 +1,24 @@
+﻿using ANK_9.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANK_9.Entities
+{
+    public class Food:BaseEntity
+    {
+        public string FoodName { get; set; }
+        public string Description { get; set; }
+        public double Calorie { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ICollection<MealFood> MealFoods { get; set; }
+        public Food()
+        {
+            IsActive = true;
+            MealFoods= new HashSet<MealFood>();
+        }
+    }
+}
